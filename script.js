a3ff64f92de997523fc852f6eb37cd9e68edecff
kalen pecson <kalenpecson@students.berkeley.net>
2017-11-09 11:38:01 -0800

    h

--- script.js
+++ script.js
@@ -1,72 +1,42 @@
-function startGameEasy(){
-    var randomwordseasy=["relax","cane","pure","chin","bare","tune","peak","try","van","harsh","quit","sport","model","table","bill","beach","lover","tank","car","seal","bow","pony","waist","habit","fool","tempt","tidy","smile","cast","agony","bond","theme","watch","wrist","gate"];
-    var randomwordnumber= Math.floor(Math.random()*randomwordseasy.length);
-    var selectedword=randomwordseasy[randomwordnumber];
-    var wordtoguess="";
-    for(var i=0;i<selectedword.length; i++){
-        wordtoguess+="_ ";
-    }
-    document.getElementById("Word").innerHTML=wordtoguess;
-    document.getElementById("Difficulty").innerHTML= "Current Difficulty: Easy"
-
-}
-
-
-function startGameMedium(){
-    setUpLetters();
-    var randomwordmedium=["slogan","bomber","rabbit","family","tumour","Sunday","father","harass","vision","suburb","master","module","summit","agenda","reason","cherry","marble","finish","church","cancel","spider","stroke","rhythm","embark"];
-    var randomwordnumber= Math.floor(Math.random()*randomwordmedium.length);
-    var selectedword=randomwordmedium[randomwordnumber];
-    var wordtoguess="";
-    for(var i=0;i<selectedword.length; i++){
-        wordtoguess+="_ ";
-    }
-    document.getElementById("Word").innerHTML=wordtoguess;
-    document.getElementById("Difficulty").innerHTML= "Current Difficulty: Medium"
-
-}
-
-
-function startGameHard(){
-    setUpLetters();
-    var randomwordhard=["pressure","perforate","executrix","harmony","inspector","challenge","applied","disagreement","partner","offense","testify","aviation","population","environment","excavate","proposal","feather","demonstration","correspond","humanity","auditon","cooperation","familiar","exception","terminal","finance","nonsense","arrange","paragraph","parameter","prospect","jealous","gallery","emotion","photography"];
-    var randomwordnumber= Math.floor(Math.random()*randomwordhard.length);
-    var selectedword=randomwordhard[randomwordnumber];
-    var wordtoguess="";
-    for(var i=0;i<selectedword.length; i++){
-        wordtoguess+="_ ";
-    }
-    document.getElementById("Word").innerHTML=wordtoguess;
-    document.getElementById("Difficulty").innerHTML= "Current Difficulty: Hard"
-
-}
-
-
-function selectGame(){
-    var chosendifficulty= document.getElementById("newgame").value;
-    if (chosendifficulty==1){
-        startGameEasy();
-    }else if (chosendifficulty ==2){
-        startGameMedium();
-    }else if(chosendifficulty==3){
-        startGameHard();
-    }
-}
 
 function playGame(){
-    document.getElementById("")
+    setUpLetters();
+    var randomwordseasy=["relax","cane","pure","chin","bare","tune","peak","try","van","harsh","quit","sport","model","table","bill","beach","lover","tank","car","seal","bow","pony","waist","habit","fool","tempt","tidy","smile","cast","agony","bond","theme","watch","wrist","gate"];
+    var randomwordmedium=["slogan","bomber","rabbit","family","tumour","Sunday","father","harass","vision","suburb","master","module","summit","agenda","reason","cherry","marble","finish","church","cancel","spider","stroke","rhythm","embark"];
+    var randomwordhard=["pressure","perforate","executrix","harmony","inspector","challenge","applied","disagreement","partner","offense","testify","aviation","population","environment","excavate","proposal","feather","demonstration","correspond","humanity","auditon","cooperation","familiar","exception","terminal","finance","nonsense","arrange","paragraph","parameter","prospect","jealous","gallery","emotion","photography"];
+    var chosendifficulty= document.getElementById("newgame").value;
+    var randomnumber= Math.floor(Math.random()*randomwordhard.length);
+
+    var displayedletters="";
+    if (chosendifficulty==1){
+        randomword = randomwordseasy[randomnumber];
+        document.getElementById("Difficulty").innerHTML= "Current Difficulty: Easy"
+    }else if (chosendifficulty ==2){
+        randomword = randomwordmedium[randomnumber];
+        document.getElementById("Difficulty").innerHTML= "Current Difficulty: Medium"
+    }else if(chosendifficulty==3){
+        randomword = randomwordhard[randomnumber];
+        document.getElementById("Difficulty").innerHTML= "Current Difficulty: Hard"
+    }
+    for(var i=0;i<randomword.length; i++){
+        displayedletters+="_ ";
+    }
+    document.getElementById("Word").innerHTML=displayedletters;
 }
+
+var randomword="";
 
 function setUpLetters(){
     var alphabet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
     for(var i=0; i<26; i++){
         var letter = alphabet[i];
-        document.getElementById("letters").innerText+= "<option value=" + letter +">"+ letter+ "</option>";
+        document.getElementById("letters").innerHTML += "<option value=" + letter +">"+ letter+ "</option>";
     }
 }
 
-
-function rightorwrong(){
-    var guess= document.getElementById("letters").value;
-
+function checkLetter(){
+    var inputletter=document.getElementById("letters").value;
+    var stringrandomword= [randomword];
+    if (randomword.)
 }
+
